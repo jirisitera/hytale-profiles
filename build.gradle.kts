dce0b7c4f7659a621459ba25cb754663d09e32e2
jirisitera <78226565+jirisitera@users.noreply.github.com>
2026-02-02 01:31:29 +0100

    style: use hytale maven repo instead of local server file

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,21 +4,24 @@ plugins {
     id("com.japicraft.hytale") version "2.2"
 }
 group = "com.japicraft"
-version = "1.0"
+version = "1.0.0"
 repositories {
     mavenCentral()
+    maven {
+        url = uri("https://maven.hytale.com/release")
+    }
 }
 dependencies {
-    compileOnly(files("run/Server/HytaleServer.jar"))
+    compileOnly("com.hypixel.hytale:Server:2026.01.28-87d03be09")
 }
 tasks {
     shadowJar {
-        archiveBaseName.set("Profiles")
+        archiveBaseName.set("profiles")
         archiveVersion.set("")
         archiveClassifier.set("")
     }
     runServer {
-        buildLocation.set("build/libs/Profiles.jar")
+        buildLocation.set("build/libs/profiles.jar")
         xmx.set("3G")
         xms.set("3G")
     }
