356b8e459a2cd708558f789505e855e3e156a35c
jirisitera <78226565+jirisitera@users.noreply.github.com>
2026-02-10 02:50:07 +0400

    feat: prepare for 1.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,13 +4,13 @@ plugins {
     id("com.japicraft.hytale") version "2.2"
 }
 group = "com.japicraft"
-version = "1.0.0"
+version = "1.1.0"
 repositories {
     mavenCentral()
     maven { url = uri("https://maven.hytale.com/release") }
 }
 dependencies {
-    compileOnly("com.hypixel.hytale:Server:2026.01.28-87d03be09")
+    compileOnly("com.hypixel.hytale:Server:2026.02.06-aa1b071c2")
 }
 tasks {
     shadowJar {
