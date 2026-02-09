bd4c5aacc6b139e998138270968a2f8d92b06aa4
jirisitera <78226565+jirisitera@users.noreply.github.com>
2026-02-10 02:50:24 +0400

    fix: improve command async handling

--- src/main/java/com/japicraft/command/ProfileCommand.java
+++ src/main/java/com/japicraft/command/ProfileCommand.java
@@ -3,36 +3,31 @@ package com.japicraft.command;
 import com.hypixel.hytale.component.Ref;
 import com.hypixel.hytale.component.Store;
 import com.hypixel.hytale.protocol.GameMode;
-import com.hypixel.hytale.server.core.Message;
 import com.hypixel.hytale.server.core.command.system.CommandContext;
-import com.hypixel.hytale.server.core.command.system.basecommands.AbstractTargetPlayerCommand;
+import com.hypixel.hytale.server.core.command.system.basecommands.AbstractPlayerCommand;
 import com.hypixel.hytale.server.core.entity.entities.Player;
 import com.hypixel.hytale.server.core.universe.PlayerRef;
 import com.hypixel.hytale.server.core.universe.world.World;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import com.japicraft.pages.ProfilePage;
-import org.checkerframework.checker.nullness.compatqual.NonNullDecl;
-import org.checkerframework.checker.nullness.compatqual.NullableDecl;
 
-public class ProfileCommand extends AbstractTargetPlayerCommand {
+import javax.annotation.Nonnull;
+import java.util.concurrent.CompletableFuture;
+
+public class ProfileCommand extends AbstractPlayerCommand {
     public ProfileCommand() {
-        super("profile", "Opens a player's profile.");
+        super("profile", "Opens your social profile.");
+        this.addAliases("prof", "pr");
         this.setPermissionGroup(GameMode.Adventure);
     }
     @Override
-    protected void execute(@NonNullDecl CommandContext commandContext, @NullableDecl Ref<EntityStore> ref, @NonNullDecl Ref<EntityStore> ref1, @NonNullDecl PlayerRef playerRef, @NonNullDecl World world, @NonNullDecl Store<EntityStore> store) {
-        if (!commandContext.isPlayer()) {
-            commandContext.sendMessage(Message.raw("This command is for players only."));
-        }
-        Ref<EntityStore> senderRef = commandContext.senderAsPlayerRef();
-        if (senderRef == null) {
-            return;
-        }
-        Store<EntityStore> senderStore = senderRef.getStore();
-        Player playerSender = senderStore.getComponent(senderRef, Player.getComponentType());
-        if (playerSender == null) {
-            return;
-        }
-        playerSender.getPageManager().openCustomPage(senderRef, senderStore, new ProfilePage(playerRef, senderRef));
+    protected void execute(@Nonnull CommandContext commandContext, @Nonnull Store<EntityStore> store, @Nonnull Ref<EntityStore> ref, @Nonnull PlayerRef playerRef, @Nonnull World world) {
+        CompletableFuture.runAsync(() -> {
+            Player player = store.getComponent(ref, Player.getComponentType());
+            if (player == null) {
+                return;
+            }
+            player.getPageManager().openCustomPage(ref, store, new ProfilePage(playerRef));
+        }, world);
     }
 }
