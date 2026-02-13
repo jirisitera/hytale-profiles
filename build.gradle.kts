ba0eea494f76212903a046aeef439ab5420d5352
jirisitera <78226565+jirisitera@users.noreply.github.com>
2026-02-13 22:05:49 +0100

    chore: simplify and improve configurations

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("java")
     id("com.gradleup.shadow") version "9.3.1"
-    id("com.japicraft.hytale") version "2.2"
 }
 group = "com.japicraft"
 version = "1.1.0"
@@ -10,17 +9,13 @@ repositories {
     maven { url = uri("https://maven.hytale.com/release") }
 }
 dependencies {
-    compileOnly("com.hypixel.hytale:Server:2026.02.06-aa1b071c2")
+    compileOnly("com.hypixel.hytale:Server:+")
 }
 tasks {
     shadowJar {
+        minimize()
         archiveBaseName.set("profiles")
-        archiveVersion.set("")
+        archiveVersion.set(version.toString())
         archiveClassifier.set("")
-    }
-    runServer {
-        buildLocation.set("build/libs/profiles.jar")
-        xmx.set("3G")
-        xms.set("3G")
     }
 }
